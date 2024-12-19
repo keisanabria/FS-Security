ee6758264b33740b961fe108ea02c926f30b97eb
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-12-19 10:56:09 -0500

    added map method

--- src/data/data_process.py
+++ src/data/data_process.py
@@ -1,5 +1,5 @@
-
 from .data_pull import DataPull
+import plotly.express as px
 import geopandas as gpd
 import pandas as pd
 import os
@@ -34,5 +34,14 @@ class DataGeo(DataPull):
         food = gpd.GeoDataFrame(food, geometry="geometry")
         return food
 
-    def pr_map(self):
-        pass
+    def pr_map(self) -> px.choropleth_mapbox:
+        gdf = self.pr_merge()
+        fig = px.choropleth_map(gdf, geojson=gdf.geometry, locations=gdf.index, color='total_calories',
+                           color_continuous_scale="Viridis",
+                           map_style="carto-positron",
+                           range_color=(0,10**7),
+                           labels={'total_calories':'Total Calaries'},
+                           center={"lat": 18.2606, "lon": -66.3962},
+                           zoom=7.5)
+        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        return fig
